ec2449c5d41dcb5aebad2c722af6b86020e504c6
lucastercas <lucasmtercas@gmail.com>
2021-10-13 20:20:10 -0300

    task(outputs): Add output of private and public IPs

--- how-to-use-this-module/outputs.tf
+++ how-to-use-this-module/outputs.tf
@@ -1,1 +1,3 @@
-
+output "compute_cluster_gcp_how_to_use" {
+  value = module.iac-modulo-compute-cluster-gcp[*].compute_cluster_gcp[*]
+}
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,7 @@
-
+output "compute_cluster_gcp" {
+  value       = {
+    client-node = module.compute_gcp[*].client-node
+    server-node = module.compute_gcp[*].server-node
+  }
+  description = "The Public and Private IPs of client and server nodes"
+}
